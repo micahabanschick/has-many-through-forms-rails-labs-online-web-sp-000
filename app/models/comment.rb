956d6744f9919ea72bbda00eaef297ec37208285
Micah Banschick <micha.ban@gmail.com>
2020-12-12 22:34:40 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def users_attributes=(users_attributes)
+    users_attributes.values.each do |users_attribute|
+      users = User.find_or_create_by(users_attribute)
+      self.post_users.build(username: user)
+    end
+  end
+
 end
