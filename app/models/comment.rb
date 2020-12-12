7732930057dcbb62631df15faffb0d2edf12ac36
Micah Banschick <micha.ban@gmail.com>
2020-12-12 22:38:06 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,8 +4,8 @@ class Comment < ActiveRecord::Base
 
   def users_attributes=(users_attributes)
     users_attributes.values.each do |users_attribute|
-      users = User.find_or_create_by(users_attribute)
-      self.post_users.build(user: user)
+      user = User.find_or_create_by(users_attribute)
+      self.user = user
     end
   end
 
