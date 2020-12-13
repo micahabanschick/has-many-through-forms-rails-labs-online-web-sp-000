fddc859d204e13faa7cd37155d3adab6cf81314f
Micah Banschick <micha.ban@gmail.com>
2020-12-13 01:09:39 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
+
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
