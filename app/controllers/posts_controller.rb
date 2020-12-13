84d60ec96652e88b2514cab8f2926d8536a38d47
Micah Banschick <micha.ban@gmail.com>
2020-12-13 02:47:24 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    # @comment = @post.comments.build
   end
 
   def index
@@ -12,7 +13,9 @@ class PostsController < ApplicationController
   end
 
   def create
+    # binding.pry
     post = Post.create(post_params)
+
     redirect_to post
   end
 
