f88f028ac6433ada8c03a1130c9bc90773273fe9
Micah Banschick <micha.ban@gmail.com>
2020-12-13 01:03:01 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,11 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
 
   def categories_attributes=(category_attributes)
-    # category_attributes.values.each do |category_attribute|
-    #   category_attribute ? category = Category.find_or_create_by(category_attribute) : nil
-    #   self.categories << category
-    # end
-    self.categories.build(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category_attribute ? category = Category.find_or_create_by(category_attribute) : nil
+      self.categories << category
+    end
+    # self.categories.build(category_attributes)
   end
 
   def categories_attributes
